f5221ea7be53ddfe993bbeeda75b07684218cdb1
Aqeel <aqeelmemon46@gmail.com>
2025-05-25 12:19:16 +0530

    Add client-side layout and providers for React Query

---
+++ src/components/ClientLayout.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Providers from './Providers';
+
+export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Providers>
+      {children}
+    </Providers>
+  );
+} 
